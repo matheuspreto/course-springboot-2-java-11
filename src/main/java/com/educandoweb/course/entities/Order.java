76c322f9045d2bc74e23cf59fc10ad7c613da6f0
matheuspreto <matheusmarcos17@hotmail.com>
2022-08-09 14:49:35 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -21,8 +23,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
+	
+		
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -41,6 +44,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set <OrderItem> items = new HashSet <>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payament payament;
 	
 
 	public Order() {
@@ -88,6 +94,15 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	
+	public Payament getPayament() {
+		return payament;
+	}
+
+	public void setPayament(Payament payament) {
+		this.payament = payament;
 	}
 
 	public Set<OrderItem> getItems(){
